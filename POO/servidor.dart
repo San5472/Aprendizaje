f4b815902d649b2c3b7a209e3d53961dd2cb7049
san5472 <santiagozambrano751@gmail.com>
2024-10-15 16:55:49 -0500

    listo

---
+++ POO/servidor.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class User{
+  int? userId; 
+  int? id; 
+  String? title;
+  String? body;
+
+  User (String s){
+    Map<String, dynamic> map = jsonDecode(s);
+    this.userId = map['userId'];
+    this.id = map['id'];
+    this.title = map ['title'];
+    this.body = map ['body']; 
+
+  }
+}
+
+void main() async{
+  var url = Uri.https('jsonplaceholder.typicode.com','/posts/1');
+  print('calculando');
+   var response = await http.get (url);
+}
