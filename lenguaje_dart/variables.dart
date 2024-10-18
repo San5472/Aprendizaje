1eec0e3ee28741028e701da94794b87a7302898d
san5472 <santiagozambrano751@gmail.com>
2024-10-17 21:53:11 -0500

    variables por revisar

---
+++ lenguaje_dart/variables.dart
@@ -0,0 +1,9 @@
+void main (){
+  String nombres = "santiago david";
+  String apellidos = "zambrano izaquita";
+  int edad = 18; 
+  String ciudad = "Barranquilla"; 
+  String educacion = "SENA";
+        
+  print ("hola me llamo $nombres $apellidos, tengo $edad, actualmente vivo en la ciudad de $ciudad y estudio en el $educacion");
+}
