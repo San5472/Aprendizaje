47d1e1aaf65f9fd7c9091f4a9023836725a0a66d
san5472 <santiagozambrano751@gmail.com>
2024-10-18 10:49:18 -0500

    Cambios realizados a codigo viejo

--- lenguaje_dart/clases.dart
+++ lenguaje_dart/clases.dart
@@ -4,35 +4,53 @@ class Carro {
  String? marca;
  String? modelo;
  double? tamanio;  
+
+ Carro ({required this.peso, required this.marca, required this.modelo, required this.tamanio});
+ @override
+  String toString() {
+    return 'Carro $peso, $marca, $modelo,  $tamanio';
+  }
 }
 class Celular {
  double? peso; 
  String? color;
- String? marca; 
+ String? marca;
+
+ Celular({required this.peso, required this.color, this.marca}); 
+ @override
+  String toString() {
+    return 'Celular $peso, $marca, $color';  
+  }
 }
 class Ventana {
   int? tamanio;
   int? ancho;
   String? color;
+
+  Ventana({required this.tamanio, required this.ancho, required this.color}); 
+
+  @override
+  String toString() {
+    return 'Ventana $tamanio, $ancho, $color';
+  }
 }
 
-void main () { 
-  Carro carro = Carro (){ 
-    Carro.peso = 1.200;
-    Carro.marca = "toyota"; 
-    Carro.modelo = "hilux"; 
-    Carro.tamanio = 3; 
+void main () {
+  Carro carro = new Carro(
+    peso: 1.200,
+    marca: "toyota",
+    modelo: "hilux",
+    tamanio: 3.0,
+  );
+  Celular celular = new Celular( 
+    peso: 2.0,
+    color: "rojo",
+    marca: "huawei"
+  );
+
+  Ventana ventana = new Ventana(tamanio: 20, ancho: 10, color: "azul");
   
-
-
-    Celular b  = Celular();
-    Celular.peso = 2.5; 
-    Celular.color = "blanco"; 
-    Celular.marca ="huawei";
-
-    Ventana c =  Ventana();
-    Ventana.tamanio = 17;
-    Ventana.ancho = 2;
-    Ventana.color = "verde claro";
-  }    
-}
+  print(celular);
+  print(carro);
+  print(ventana);
+} 
