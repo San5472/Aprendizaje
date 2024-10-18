bf788d89fdad76abd30b3655b508a89e5cbc5c03
San5472 <166321055+San5472@users.noreply.github.com>
2024-10-18 14:21:17 -0500

    Update variables.dart

--- lenguaje_dart/variables.dart
+++ lenguaje_dart/variables.dart
@@ -4,6 +4,6 @@ void main (){
   int edad = 18; 
   String ciudad = "Barranquilla"; 
   String educacion = "SENA";
-        
+  double resultado = 3.0; 
   print ("hola me llamo $nombres $apellidos, tengo $edad, actualmente vivo en la ciudad de $ciudad y estudio en el $educacion");
 }
