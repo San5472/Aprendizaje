5f4716c32017c76c392c7a41517280c2c9e3f720
San5472 <santiagozambrano751@gmail.com>
2024-11-20 17:11:47 -0500

    faltan algunas cosas

---
+++ html/basico.js
@@ -0,0 +1,37 @@
+
+// esto es una alerta
+alert("joa que monda");
+
+// uso de las variables
+let nombre = "santiago";
+let apellido = "zambrano";
+
+// imprimir por consola
+console.log(nombre);
+
+// uso de las constantes
+
+const altura = 180;
+console.log(altura);
+
+// en el uso de las constantes son cajas por asi decirlo que no se le puede cambiar el nombre, ya es un valor definido 
+
+// concatenacion
+
+let concatenacion = nombre + apellido;
+
+// seleccionar elementos de la pagina
+
+let datos = document.querySelector("#datos");
+datos.innerHTML = `
+    <h1>caja de datos</h1>
+    <h2>el nombre  es ${nombre} ${apellido}</h2>
+`;    
+
+// condicionales
+
+if (altura >= 200){
+    datos.innerHTML += "<h1>eres una persona alta</h1>";
+} else {
+    datos.innerHTML += "<h1>eres una persona baja</h1>";
+}
