4a3eb32f28b8697cffed1331ba9dd1944e23aad0
San5472 <santiagozambrano751@gmail.com>
2025-01-03 15:22:50 -0500

    Datos primitivo

---
+++ javaScript/control.js
@@ -0,0 +1,29 @@
+// Datos primitivos: en este tipo de datos se le puede poner cualquier nombre al dato que se quiera poner
+
+let nombre = "santiago";
+
+// Datos primitivos
+
+// tipo de dato undefined, se llama asi cuando no se le añade el valor, al tipo de dato
+let juego;
+
+// boolean tipo de dato verdadero o falso.
+
+let boolean = true;
+
+// number dato que acepta cualquier tipo numerico
+
+let edad = 18;
+let año = 2025; 
+
+// beiglnt tipo de dato numerico que acepta numeros grandes
+
+let pago = 1123020252522121102;
+
+// dato String: cadena de texto
+
+let texto = "JavaScript";
+
+// dato symbol: este es un tipo de dato privado, en el cual se le añade una informacion, dicha informacion no puede ser remplazada
+
+let Syml = Symbol();
