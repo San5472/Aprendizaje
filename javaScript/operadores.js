4e71bc646ea7d25f18a19cef961d6296040345ab
San5472 <santiagozambrano751@gmail.com>
2025-01-15 15:16:04 -0500

    operadores

---
+++ javaScript/operadores.js
@@ -0,0 +1,17 @@
+// estos tipos de operadores sirven para operaciones sobre valores
+
+// operadores aritmaticos
+
+// modulo %
+// exponenciacion ** 
+// incremento ++
+// decremento --
+let a = 2, b = 3; 
+let c = 8, d = 16;
+let resta = 5, res = 5;
+let multiplicacion = 10, mul = 5;
+
+console.log(a+b);
+console.log(c/d);
+console.log(resta-res);
+console.log(multiplicacion * mul);
