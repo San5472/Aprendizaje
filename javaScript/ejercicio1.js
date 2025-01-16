cf3ca8a43e8b4b1611dd109d47489d2548356b16
San5472 <santiagozambrano751@gmail.com>
2025-01-16 14:56:01 -0500

    finalizado/ comentarios añadidos

--- javaScript/ejercicio1.js
+++ javaScript/ejercicio1.js
@@ -1,22 +1,22 @@
 // ejercicio de prueba
 
-const readline = require('readline');
+const readline = require('readline'); // ingreso por terminal
 
 console.log('bienvenido');
 
-const ingresoContraseña = readline.createInterface({
-    input:process.stdin,
-    output: process.stdout
+const ingresoContraseña = readline.createInterface({ // entrada y salida, permite la entrada y salida desde la terminal 
+    input:process.stdin, // entrada
+    output: process.stdout // salida
 });
 
 
-ingresoContraseña.question('ingrese su contraseña:', (Contraseña) =>{
-    if (Contraseña == "lapiz"){
+ingresoContraseña.question('ingrese su contraseña:', (Contraseña) =>{ // condicional
+    if (Contraseña == "lapiz"){ // primer valor si es correcto me va a retornar true 
         console.log(true)
-    } else if (Contraseña == "hola") {
+    } else if (Contraseña == "hola") { // lo mismo si es correcto devuelve true
         console.log(true)
     } else {
-        console.log(false)
+        console.log(false) // en caso que sea incorrecto me debe de devolver false
     }
-    ingresoContraseña.close();
+    ingresoContraseña.close(); // cierre de la condicion 
 });
