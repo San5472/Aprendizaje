90dd18de215c5f4f3a41ec046b6ce72bb8830cb3
San5472 <santiagozambrano751@gmail.com>
2025-01-18 15:59:37 -0500

    colocando resultados

--- javaScript/ejercicio3.js
+++ javaScript/ejercicio3.js
@@ -25,13 +25,13 @@ interfaz.question("Bienvenido, ingrese su peso:", (peso) =>{ // uso del condicio
         const resultado = peso / (altura*altura);
 
         if (resultado <= 18.5){
-        console.log("Estas bajo de peso, su resultado es:", resultado);
+        console.log("Estas bajo de peso, su resultado es:",resultado);
         } else if (resultado <= 24.9) {
-        console.log("Esta es un peso normal su resultado es:",); 
+        console.log("Esta es un peso normal su resultado es:",resultado ); 
         } else if (resultado <= 29.9) {
-        console.log("Estas en sobre peso"); 
+        console.log("Estas en sobre peso su resultado es:",resultado); 
         } else if  (resultado => 30) {
-        console.log("Estas obeso compae'");
+        console.log("Estas obeso su resultado es:",resultado);
         }
 
         interfaz.close();
