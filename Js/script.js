1039ca261b9e43ba51affea2b0a6c17468b65cb1
San5472 <santiagozambrano751@gmail.com>
2025-05-14 17:27:13 -0500

    Añadido
    Mas cosas que se necesitaba aprender

--- Js/script.js
+++ Js/script.js
@@ -26,3 +26,64 @@ if (animal.toLocaleLowerCase() === "vaca"){
 } else {
     console.log("Imitas los sonidos")
 }
+
+// arrow function
+
+function saludar (nombre){
+    return "hola " + nombre
+}
+
+console.log(saludar("david"))
+
+
+// funcion anonima
+
+
+
+const saludo = function (nombre) {
+    return 'Hola ' + nombre; 
+}
+
+console.log(saludo("zambrano"))
+
+// funciones flechas
+
+const lastname = apellido => 'saludos ' + apellido;
+
+console.log(lastname("izaquita"));
+
+const suma = (a,b) => a + b;  // se tendria que definir las variables
+
+const resultado = suma (1,6);
+
+console.log(`Este es el resultado: ${resultado}`);
+
+
+// el destructuring me sirve para poder extraer datos de una clase o de arrays
+
+const Casa = {
+    material1: "Cemento",
+    material2: "Bloque",
+    material3: "Arena"
+};
+
+const { material1, material2, material3 } = Casa;
+
+console.log(material1, material2,material3);
+
+// arrow con un array
+
+const colores = ["rojo", "verde", "azul"];
+const [a,b,c] = colores;
+
+console.log(a,b,c);
+
+const Carro = {
+    placa: "234",
+    color: "negro",
+    modelo: "Fortuner"
+}
+
+const {placa, color,modelo} = Carro;
+
+console.log(placa,color,modelo);
